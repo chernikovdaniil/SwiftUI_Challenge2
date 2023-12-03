ab885799ad87a9c8c023a490bd925517ae9d3729
Danil Chernikov <danil.chernikov@onix-systems.com>
2023-12-03 14:18:37 +0100

    Remade displaying several texts into the single line.

--- SwiftUI_Challenge2/ContentView.swift
+++ SwiftUI_Challenge2/ContentView.swift
@@ -7,26 +7,21 @@
 
 import SwiftUI
 
+#Preview {
+    ContentView()
+}
+
 struct ContentView: View {
-    // Марафон по SwiftUI
-    var topText: AttributedString = {
-        let firstPart = "Марафон"
-        let secondPart = "по SwiftUI"
-        
-        var attributedString = AttributedString("Марафон по SwiftUI")
-        attributedString.font = .system(size: 13)
+    var text: AttributedString = {
+        var attributedString = AttributedString("Марафон по SwiftUI <<Отцовский пинок>>")
         attributedString.foregroundColor = .black
         
         let firstPartRange = attributedString.range(of: "Марафон")!
         attributedString[firstPartRange].foregroundColor = .gray
         
-        return attributedString
-    }()
-    
-    var bottomText: AttributedString = {
-        var attributedString = AttributedString(#"<<Отцовский пинок>>"#)
-        attributedString.font = .system(size: 24, weight: .bold, design: .default)
-        attributedString.foregroundColor = .blue
+        let secondPartRange = attributedString.range(of: "<<Отцовский пинок>>")!
+        attributedString[secondPartRange].font = .system(size: 26, weight: .bold, design: .default)
+        attributedString[secondPartRange].foregroundColor = .blue
         
         return attributedString
     }()
@@ -36,21 +31,15 @@ struct ContentView: View {
     var body: some View {
         VStack {
             VStack(alignment: .leading) {
-                Text(topText)
-                Text(bottomText)
+                Text(text)
             }
-            .frame(height: 150)
+            .frame(minHeight: 200)
             .fixedSize(horizontal: false, vertical: true)
-            .frame(width: rectangleWidth)
+            .frame(width: rectangleWidth, alignment: .leading)
             .border(.red.opacity(0.45), width: 1)
             
-            Slider(value: $rectangleWidth,
-                   in: 100...300) { _ in }
+            Slider(value: $rectangleWidth, in: 100...300)
+                .padding()
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
